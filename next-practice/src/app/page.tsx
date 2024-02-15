3108353722b0e03e5c46a5e62335c05aa4603ecb
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-02-15 23:43:34 +0900

    ✨ fetch forcast data

--- next-practice/src/app/page.tsx
+++ next-practice/src/app/page.tsx
@@ -1,11 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ForcastData {
+    publishingOffice: string;
+    reportDatetime: string;
+    targetArea: string;
+    headlineText: string;
+    text: string;
+}
+
 export default function Home() {
+    let area: String[] = ["山梨県", "長野県", "岐阜県", "静岡県", "愛知県"];
+    const [idx, setIdx] = useState<number>(0);
+    const inc = () => {
+        setIdx((idx + 1) % area.length);
+    };
+    const dec = () => {
+        setIdx((idx + area.length - 1) % area.length);
+    };
+
+    const [data, setData] = useState<ForcastData>();
+
+    useEffect(() => {
+        const apiUrl =
+            "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/220000.json";
+        const fetchData = async () => {
+            try {
+                const response = await fetch(apiUrl);
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <h1>タイトル</h1>
-            <h2>サブタイトル1</h2>
-            <p>ないよういろいろ</p>
-            <h2>サブタイトル2</h2>
-            <p>ないよういろいろ</p>
+            <p>{area[idx]}</p>
+            <p>{data && data.text}</p>
+            <button onClick={inc}>+</button>
+            <button onClick={dec}>-</button>
         </div>
     );
 }
